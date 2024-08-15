72ec598839383d41b437b46488c82047738397fe
abhiishekpanchal <abhishekp.280403@gmail.com>
2024-08-16 02:45:58 +0530

    created upate listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -12,11 +12,10 @@ export const createListing = async (req, res, next) => {
 
 export const deleteListing = async (req, res, next) => {
     const listing = await Listing.findById(req.params.id);
-    
+
     if(!listing) {
         return next(errorHandler(404, "Listing not found!"));
     }
-
     if (req.user.id !== listing.userRef) {
         return next(errorHandler(401, "You do not have permission to delete this listing!"));
     }
@@ -28,3 +27,25 @@ export const deleteListing = async (req, res, next) => {
         next(error);
     }
 };
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing) {
+        return next(errorHandler(404, "Listing not found!"));
+    }
+    if(req.user.id !== listing.userRef) {
+        return next(errorHandler(401, "You do not have permission to update this listing!"));
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+            );
+        return res.status(200).json(updatedListing);
+    } catch(error) {
+        next(error);
+    }
+}
