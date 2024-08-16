699a68f14c49a1f864892e1bbe97af7f0a4467af
abhiishekpanchal <abhishekp.280403@gmail.com>
2024-08-17 00:57:18 +0530

    completed listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -202,7 +202,9 @@ function CreateListing() {
                         />
                         <div className='flex flex-col items-center'>
                             <p>Regular price</p>
+                            {formData.type === 'rent' && (
                             <span className='text-xs'>($ / month)</span>
+                            )}
                         </div>
                     </div>
                     {formData.offer && (
@@ -213,7 +215,9 @@ function CreateListing() {
                             />
                             <div className='flex flex-col items-center'>
                                 <p>Discounted price</p>
-                                <span className='text-xs'>($ / month)</span>
+                                {formData.type === 'rent' && (
+                                    <span className='text-xs'>($ / month)</span>
+                                )}
                             </div>
                         </div>
                     )}
--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -219,7 +219,9 @@ function CreateListing() {
                         />
                         <div className='flex flex-col items-center'>
                             <p>Regular price</p>
+                            {formData.type === 'rent' && (
                             <span className='text-xs'>($ / month)</span>
+                            )}
                         </div>
                     </div>
                     {formData.offer && (
@@ -230,7 +232,9 @@ function CreateListing() {
                             />
                             <div className='flex flex-col items-center'>
                                 <p>Discounted price</p>
-                                <span className='text-xs'>($ / month)</span>
+                                {formData.type === 'rent' && (
+                                    <span className='text-xs'>($ / month)</span>
+                                )}
                             </div>
                         </div>
                     )}
