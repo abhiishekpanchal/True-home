2ca4ae9ef481e127babad7456a625a3859e988f6
abhiishekpanchal <abhishekp.280403@gmail.com>
2024-08-18 01:43:32 +0530

    fixed ListingItem size

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -5,10 +5,10 @@ import { MdLocationOn } from 'react-icons/md'
 function ListingItem({listing}) {
   return (
     <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden 
-    rounded-lg w-full sm:w-[400px]'>
+    rounded-lg w-full sm:w-[330px]'>
         <Link to={`/listing/${listing._id}`}>
             <img src={listing.imageUrls[0]} alt="listing cover" 
-            className='h-[320px] sm:h-[280px] w-full object-cover hover:scale-105 
+            className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 
             transition-scale duration-300'/>
             <div className='p-3 flex flex-col gap-2 w-full'>
                 <p className='text-lg font-semibold text-slate-700 truncate'>{listing.name}</p>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -53,15 +53,15 @@ function Home() {
     <div>
       {/* top */}
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
-        <h1 className='text-slate-700 font-bold text-4xl lg:text-8xl'>Find your next 
+        <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>Find your next 
           <span className='text-slate-500'> perfect</span> 
           <br /> place with ease
         </h1>
-        <div className='text-gray-400 text-lg sm:text-3xl'>TrueHomes is the best place to find your next perfect place to live.
+        <div className='text-gray-400 text-xs sm:text-sm'>TrueHomes is the best place to find your next perfect place to live.
           <br />
           We have a wide range of properties for you to choose from.
         </div>
-        <Link className='text-lg sm:text-2xl text-blue-800 font-bold hover:underline' to={"/search"}>Let's get started...</Link>
+        <Link className='text-xs sm:text-sm text-blue-800 font-bold hover:underline' to={"/search"}>Let's get started...</Link>
       </div>
       
       {/* swiper */}
@@ -70,7 +70,7 @@ function Home() {
           offerListings && offerListings.length > 0 && offerListings.map((listing) => (
             <SwiperSlide>
               <div style={{background: `url(${listing.imageUrls[0]}) center no-repeat`, backgroundSize: 'cover'}} 
-              className='h-[600px]' key={listing._id}>
+              className='h-[500px]' key={listing._id}>
               </div>
             </SwiperSlide>
           ))
