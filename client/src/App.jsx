73e7f90930ae983b760336f077bc7c5e6323ceca
abhiishekpanchal <abhishekp.280403@gmail.com>
2024-08-18 14:00:38 +0530

    fixed some issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,20 +14,20 @@ import Search from './pages/Search';
 
 function App() {
   return <BrowserRouter>
-  <Header />
-  <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/sign-in' element={<Signin />} />
-    <Route path='/sign-up' element={<SignUp />} />
-    <Route path='/about' element={<About />} />
-    <Route path='/search' element={<Search />} />
-    <Route path='/listing/:listingId' element={<Listing />} />
-    <Route element={<PrivateRoute />}>
-      <Route path='/profile' element={<Profile />} />
-      <Route path='/create-listing' element={<CreateListing />} />
-      <Route path='/update-listing/:listingId' element={<UpdateListing />} />
-    </Route>
-  </Routes>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/sign-in' element={<Signin />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/search' element={<Search />} />
+      <Route path='/listing/:listingId' element={<Listing />} />
+      <Route element={<PrivateRoute />}>
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/create-listing' element={<CreateListing />} />
+        <Route path='/update-listing/:listingId' element={<UpdateListing />} />
+      </Route>
+    </Routes>
   </BrowserRouter>
 }
 
